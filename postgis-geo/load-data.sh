d83f472dba56b522f781d724d6d70e7770bb7f9f
Ihor Lukianov <lukianov.igor@hotmail.com>
2025-05-12 00:27:26 +0700

    init on postgis geo article

---
+++ postgis-geo/load-data.sh
@@ -0,0 +1,11 @@
+osm2pgsql \
+    --create \
+    --verbose \
+    --database gis \
+    --latlong \
+    --number-processes 4 \
+    --username gis \
+    --host localhost \
+    --port 5433 \
+    --password \
+    gis.osm.pbf
